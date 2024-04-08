45cbca5cc2556583b1d1b739c6f38eadc08d951a
Ricardo Band <ricardo.band@idealo.de>
2024-04-08 10:45:28 +0200

    add basic flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Basic terraform and python based environment";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            awscli2
+            aws-sso-cli
+          ];
+        };
+      }
+    );
+}
+
